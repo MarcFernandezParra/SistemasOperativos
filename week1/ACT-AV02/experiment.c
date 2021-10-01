f6bf2418d706d67da80143fde0e915d063ffffd5
Jordi Mateo Fornés (UdL) <61190134+JordiMateoUdL@users.noreply.github.com>
2021-10-01 19:43:41 +0200

    Millores, explicacions al CV.

--- week1/ACT-AV02/experiment.c
+++ week1/ACT-AV02/experiment.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 long nanosec(struct timeval t) {
 	return((t.tv_sec * 1000000 + t.tv_usec) * 1000);
@@ -20,21 +21,23 @@ int main() {
 
 	if (fork() == 0) {
 		execv("/bin/echo", args);
-	}
+	}else{
+		wait(NULL);
 	
-	res = gettimeofday(&t2, NULL); assert(res == 0);
+		res = gettimeofday(&t2, NULL); assert(res == 0);
 
-	avgT1 = (nanosec(t2) - nanosec(t1)) / 2;
+		avgT1 = (nanosec(t2) - nanosec(t1)) / 2;
 
-	res = gettimeofday(&t1, NULL); assert(res == 0);
-	printf("Test PRINTF\n");
-	res = gettimeofday(&t2, NULL); assert(res == 0);
+		res = gettimeofday(&t1, NULL); assert(res == 0);
+		printf("Test PRINTF\n");
+		res = gettimeofday(&t2, NULL); assert(res == 0);
 
-	avgT2 = (nanosec(t2) - nanosec(t1)) / 2;
+		avgT2 = (nanosec(t2) - nanosec(t1)) / 2;
 
-	printf("Time for 1 : %f\n",
-	avgT1);
-	printf("Time for 2 : %f\n",
-	avgT2);
+		printf("Time for 1 : %f\n",avgT1);
+		printf("Time for 2 : %f\n",avgT2);
+		
+		exit(0);
+	}
 
 }
