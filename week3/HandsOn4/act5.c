dc44ae3edfd25938613cef49dae992d99830bbaa
MarcFernandezParra <90846967+MarcFernandezParra@users.noreply.github.com>
2021-10-22 17:00:32 +0200

    Fix randoms a memoria del pare
    A mes a mes, ara els 2 randoms donen numeros diferents, els generem per el pid del robb (encara que el primer cop sempere sera amb la seed de 0)

--- week3/HandsOn4/act5.c
+++ week3/HandsOn4/act5.c
@@ -35,45 +35,31 @@ int main(int argc, char* argv[]) {
                 // Robb Stark
                 srand(robb);
                 int cause =rand() % 2;
-                printf("%d -------------\n",cause);
                 printf("Hola sóc en Robb Stark amb pid = %d, soc un fill del matrimoni de la Catelyn Stark i Ned Stark.\n", getpid());
                 printf("Soc en Robb amb pid = %d i he estat %s\n", getpid(), causaDeMort(cause));
                 exit(cause);
             } else {
                 // Mort Ned
-                srand(ned);
+                srand(robb);
                 int cause = rand() % 2;
-                char concat[2];
+                char concat[3];
                 char robDed[1];
                 
                 waitpid(robb, &statusRobb, WUNTRACED);                
-                
-
-                char robbChar[1];
+                char robbChar[2];
                 sprintf(robbChar, "%d", statusRobb>>8);
-                printf("robbChar == %s \n", robbChar);
 
-                char nedChar[1];
+                char nedChar[2];
                 sprintf(nedChar, "%d", cause);
-                printf("nedChar == %s \n", nedChar);
+                concat[0] = robbChar[0];
+                concat[1] = nedChar[0];
+                concat[2] = nedChar[1];
+                
 
-                printf("printo els dos chars == %s .--- %s\n", &robbChar, robbChar);
-                //strcpy(concat, robbChar);
-                //strcat(concat, nedChar)
-                //printf("concat == %s\n", concat);
-
-                //waitpid(robb, 0, WUNTRACED);
                 printf("Sóc en Ned amb pid = %d i m’acaben de %s\n", getpid(), causaDeMort(cause));
-                //sprintf(values,"%s", cause);
-                //sprintf(values[1], "%d",statusRobb);
-                //strcat(values, robDed);
-
                 
-               fflush(stdout);
                 int returnExit = atoi(concat);
-                printf("return exit: %d\n", returnExit);
-                
-               fflush(stdout);
+                fflush(stdout);
                 exit(returnExit);
 
             }
@@ -81,11 +67,19 @@ int main(int argc, char* argv[]) {
         } else {
             // Mort Ricard
             waitpid(ned, &statusNed, WUNTRACED);
-            char result[2];// = atoi(statusNed>>8);
-            //sprintf(result, statusNed>>8);
-            printf("%d\n",statusNed>>8);
-            printf("%c\n",result[1]);
-            printf("En resum el meu fill Robb ha estat %s, en Ned %s i jo en Rickard amb pid = %d i m’han executat.\n", causaDeMort(statusRobb>>8), causaDeMort(statusNed>>8), getpid());
+            
+            char exitCodeStr[3];
+            char arrayRob[2];
+            char arrayNed[2];
+            sprintf(exitCodeStr, "%d", statusNed>>8);
+            exitCodeStr[2] = '\0';
+            arrayRob[0] = exitCodeStr[0];
+            arrayRob[1] = '\0';
+            arrayNed[0] = exitCodeStr[1];
+            arrayNed[1] = '\0';
+            int exitCodeRobb = atoi(arrayRob);
+            int exitCodeNed = atoi(arrayNed);
+            printf("En resum el meu fill Robb ha estat %s, en Ned %s i jo en Rickard amb pid = %d i m’han executat.\n", causaDeMort(exitCodeRobb), causaDeMort(exitCodeNed), getpid());
             exit(0);
 
         }
