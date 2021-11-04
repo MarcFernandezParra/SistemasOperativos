879e3be0a89f01a08ff344d1d88d0158ed40496d
MarcFernandezParra <maarkusmou51@gmail.com>
2021-11-04 16:35:42 +0100

    final ejercicio pokemon

--- week5/HandsOn5/act4/main.c
+++ week5/HandsOn5/act4/main.c
@@ -18,8 +18,45 @@
 #define KWHT "\x1B[37m"
  
 char *args[] = {"pokemon", "pokemon", NULL};
+
+int probabilities[9];
+char numberOfProb = 0;  
+void throwPokeball(){
+
+    srand(time(NULL));
+    int throw = (rand() % 10) + 1;
+    printf("RANDOM %d\n",throw);
+    for(int i = 0; i<=numberOfProb; i++){
+        if(probabilities[i] == throw){
+            throw = 2;
+        }
+    }
+    if (throw == 2 || throw == 7) {
+        printf("Exit\n");
+        exit(throw);   
+    }
+}
+
+void throwBerry(){
+    if(numberOfProb<9){
+        numberOfProb ++;
+        printf("S'ha augmentat la probabilitat de captura!\n");
+    }else{
+        printf("La probabilitat de captura no pot augmentar més.\n");
+    }
+}
  
 int main(int argc, char *argv[]) {
+
+    probabilities[0] = 2;
+    probabilities[1] = 1;
+    probabilities[2] = 3;
+    probabilities[3] = 4;
+    probabilities[4] = 5;
+    probabilities[5] = 6;
+    probabilities[6] = 8;
+    probabilities[7] = 9;
+    probabilities[8] = 10;
 
     int endFlag = 1, encounterEndFlag = 1;
     int childProcess, status, throw = 0;
@@ -48,21 +85,12 @@ int main(int argc, char *argv[]) {
                     childProcess = fork();
 
                     if (childProcess == 0) {
-
                         // Child process
-
+                        signal(SIGUSR1, throwPokeball);
+                        signal(SIGUSR2, throwBerry);
                         while (1) {
-
+                            
                             raise(SIGSTOP);
-
-                            srand(time(NULL));
-                            throw = (rand() % 10) + 1;
-
-                            if (throw == 2 || throw == 7) {
-                                printf("Exit\n");
-                                exit(throw);   
-                            }
-
                         }
 
                         exit(0);
@@ -82,16 +110,18 @@ int main(int argc, char *argv[]) {
                             switch(choice) {
 
                                 case 'P':
+                                    kill(childProcess, SIGUSR1);
+                                    
                                     kill(childProcess, SIGCONT);
                                     waitpid(-1, &status, WUNTRACED);
                                     
                                     status = status >> 8;
-
+                                    printf("status %d\n",status);
                                     if (status == 2) {
-                                        printf("CAPUTRAO!!!\n");
+                                        printf("Capturat!!!\n");
                                         encounterEndFlag = 0;
                                     } else if (status == 7) {
-                                        printf("ESCAPAO...\n");
+                                        printf("S'ha escapat...\n");
                                         encounterEndFlag = 0;
                                     } else {
                                         printf("Se ha salido de la bola :peeposad:\n");
@@ -100,6 +130,12 @@ int main(int argc, char *argv[]) {
                                     break;
 
                                 case 'B':
+                                
+                                    kill(childProcess, SIGUSR2);
+                                    
+                                    kill(childProcess, SIGCONT);
+                                    waitpid(-1, &status, WUNTRACED); //se vuelve a dormir el hijo
+                                    
                                     break;
 
                                 default:
@@ -108,7 +144,8 @@ int main(int argc, char *argv[]) {
                         }
 
                         wait(NULL);
-
+                        numberOfProb = 0;
+                        encounterEndFlag = 1;
                     }
 
                     break;
